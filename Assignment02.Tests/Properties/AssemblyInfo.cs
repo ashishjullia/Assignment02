4974c18a24910ddf16a0f8ce9b6fd525e7f1ae73
Ashish Jullia <ashishjullia19@gmail.com>
2020-02-21 05:04:46 -0500

    Test cases have been implemented

---
+++ Assignment02.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assignment02.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment02.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0d853c17-ecb6-48ee-8235-b93f7f2ac509")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignment02.Tests/TriangleSolverTest.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = NUnit.Framework.Assert;
+
+namespace Assignment02.Tests
+{
+    [TestFixture]
+    public class TriangleSolverTest
+    {
+        [Test]
+        public void Put222_Get_Equilateral_Triangle()
+        {
+            Assert.AreEqual("Equilateral Triangle", TriangleSolver.Analyze(2, 2, 2));
+        }
+
+        [Test]
+        public void Put000_Get_Not_Valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(0, 0, 0));
+        }
+
+        [Test]
+        public void Put220_Get_Not_Valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(2, 2, 0));
+        }
+
+        [Test]
+        public void Put224_Get_Not_valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(2, 2, 4));
+        }
+        [Test]
+        public void Put123_Get_Not_valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(1, 2, 3));
+        }
+
+        [Test]
+        public void Put2_minus23_Get_Not_Valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(2, -2, 3));
+        }
+
+        [Test]
+        public void Put467_Get_Scalene_Triangle()
+        {
+            Assert.AreEqual("Scalene Triangle", TriangleSolver.Analyze(4, 6, 7));
+        }
+
+        [Test]
+        public void Put_minus2_minus2_minus2_Get_Not_Valid_Triangle()
+        {
+            Assert.AreEqual("Not valid triangle", TriangleSolver.Analyze(-2, -2, -2));
+        }
+    }
+}
