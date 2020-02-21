e37aed058fc203f16579ecf620d9694a09648000
Ashish Jullia <ashishjullia19@gmail.com>
2020-02-21 04:54:19 -0500

    Created TriangleSolver Class

---
+++ Assignment02/TriangleSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment02
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int tSide1, int tSide2, int tSide3)
+        {
+            // checking whether the triangle is valid or not
+            if ((tSide1 + tSide2 <= tSide3) || (tSide2 + tSide3 <= tSide1) || (tSide1 + tSide3 <= tSide2))
+            {
+                return "Not valid triangle";
+            }
+            else
+            {
+                // checking the type of triangle
+                if (tSide1 == tSide2 && tSide2 == tSide3)
+                {
+                    return "Equilateral Triangle";
+                }
+                else if (tSide1 == tSide2 || tSide2 == tSide3 || tSide1 == tSide3)
+                {
+                    return "Isosceles Triangle";
+                }
+                else
+                {
+                    return "Scalene Triangle";
+                }
+            }
+        }
+    }
+}
